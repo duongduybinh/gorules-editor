94a867d0f101e4248eadea7d8fae070fb343575c
Duong Duy Binh <duongduybinh.dev@gmail.com>
2025-09-01 01:36:14 +0700

    Integrate upstream fork and add @gorules/editor to monorepo

--- packages/lezer-zen/rollup.config.js
+++ packages/lezer-zen/rollup.config.js
@@ -1,4 +1,5 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import path from 'path';
 
 export default {
   input: './src/parser.js',
@@ -7,7 +8,17 @@ export default {
     { format: 'es', file: './dist/index.js' },
   ],
   external(id) {
-    return !/^[\.\/]/.test(id);
+    // return !/^[\.\/]/.test(id);
+    // Đường dẫn tuyệt đối (Windows: C:\..., Linux/macOS: /...)
+    const isAbsolute = path.isAbsolute(id);
+    // Đường dẫn tương đối (./ or ../)
+    const isRelative = id.startsWith('.');
+
+    // Nếu là local file (relative hoặc absolute) thì không external
+    if (isAbsolute || isRelative) return false;
+
+    // Ngược lại (package trong node_modules) thì external
+    return true;
   },
   plugins: [nodeResolve()],
 };
